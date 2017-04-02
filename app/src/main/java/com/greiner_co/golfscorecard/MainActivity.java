9fc69fde957f13d0adf6b5e579df433a47c96590
Jens Greiner <jens.greiner@me.com>
2017-04-02 14:45:14 +0200

    removed lock to portrait and added state saving methods to enable switch between portrait and landscape without losing the current state of each textview value and color

--- app/src/main/java/com/greiner_co/golfscorecard/MainActivity.java
+++ app/src/main/java/com/greiner_co/golfscorecard/MainActivity.java
@@ -9,17 +9,76 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int[] myHoleScoreViews = {R.id.playerHole1, R.id.playerHole2, R.id.playerHole3, R.id.playerHole4, R.id.playerHole5, R.id.playerHole6, R.id.playerHole7, R.id.playerHole8, R.id.playerHole9, R.id.playerHole10, R.id.playerHole11, R.id.playerHole12, R.id.playerHole13, R.id.playerHole14, R.id.playerHole15, R.id.playerHole16, R.id.playerHole17, R.id.playerHole18};
-    private final int[] myHoleParViews = {R.id.parHole1, R.id.parHole2, R.id.parHole3, R.id.parHole4, R.id.parHole5, R.id.parHole6, R.id.parHole7, R.id.parHole8, R.id.parHole9, R.id.parHole10, R.id.parHole11, R.id.parHole12, R.id.parHole13, R.id.parHole14, R.id.parHole15, R.id.parHole16, R.id.parHole17, R.id.parHole18};
+    private static final String HOLE_VALUES = "scoreOfHoles";
+    private static final String PLAYER_TOTAL_SCORE = "totalPlayerScore";
+    private static final String HOLE_SCORE_VIEWS = "holeScoreViews";
+    private static final String HOLE_PAR_VIEWS = "holeParViews";
+    private static final String HOLE_SCORE_VIEW_COLORS = "holeScoreViewColors";
+    private int[] myHoleScoreViews = {R.id.playerHole1, R.id.playerHole2, R.id.playerHole3, R.id.playerHole4, R.id.playerHole5, R.id.playerHole6, R.id.playerHole7, R.id.playerHole8, R.id.playerHole9, R.id.playerHole10, R.id.playerHole11, R.id.playerHole12, R.id.playerHole13, R.id.playerHole14, R.id.playerHole15, R.id.playerHole16, R.id.playerHole17, R.id.playerHole18};
+    private int[] myHoleParViews = {R.id.parHole1, R.id.parHole2, R.id.parHole3, R.id.parHole4, R.id.parHole5, R.id.parHole6, R.id.parHole7, R.id.parHole8, R.id.parHole9, R.id.parHole10, R.id.parHole11, R.id.parHole12, R.id.parHole13, R.id.parHole14, R.id.parHole15, R.id.parHole16, R.id.parHole17, R.id.parHole18};
     private int playerTotalScore = 0;
     private int[] myHoleValues = new int[18];
+    private int[] myHoleScoreViewColors = new int[18];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        reset(null);
+        if (savedInstanceState != null) {
+            myHoleScoreViews = savedInstanceState.getIntArray(HOLE_SCORE_VIEWS);
+            myHoleParViews = savedInstanceState.getIntArray(HOLE_PAR_VIEWS);
+            myHoleValues = savedInstanceState.getIntArray(HOLE_VALUES);
+            playerTotalScore = savedInstanceState.getInt(PLAYER_TOTAL_SCORE);
+            displayTotalForPlayer();
+            int color = 0;
+            for (int i = 0; i < myHoleScoreViews.length; i++) {
+                if (myHoleScoreViewColors != null) {
+                    color = myHoleScoreViewColors[i];
+                }
+                displayScoreForHole(myHoleValues[i], myHoleScoreViews[i], color);
+            }
+        } else {
+            reset(null);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // save the current state of the counters and ui
+        savedInstanceState.putIntArray(HOLE_VALUES, myHoleValues);
+        savedInstanceState.putInt(PLAYER_TOTAL_SCORE, playerTotalScore);
+        savedInstanceState.putIntArray(HOLE_SCORE_VIEWS, myHoleScoreViews);
+
+        for (int i = 0; i < myHoleScoreViews.length; i++) {
+            TextView view = (TextView) findViewById(myHoleScoreViews[i]);
+            int color = view.getCurrentTextColor();
+            myHoleScoreViewColors[i] = color;
+        }
+        savedInstanceState.putIntArray(HOLE_SCORE_VIEW_COLORS, myHoleScoreViewColors);
+
+        savedInstanceState.putIntArray(HOLE_PAR_VIEWS, myHoleParViews);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // restore state members from saved instance
+        myHoleScoreViews = savedInstanceState.getIntArray(HOLE_SCORE_VIEWS);
+        myHoleParViews = savedInstanceState.getIntArray(HOLE_PAR_VIEWS);
+        playerTotalScore = savedInstanceState.getInt(PLAYER_TOTAL_SCORE);
+        myHoleValues = savedInstanceState.getIntArray(HOLE_VALUES);
+        myHoleScoreViewColors = savedInstanceState.getIntArray(HOLE_SCORE_VIEW_COLORS);
+        int color = 0;
+        for (int i = 0; i < myHoleScoreViews.length; i++) {
+            if (myHoleScoreViewColors != null) {
+                color = myHoleScoreViewColors[i];
+            }
+            displayScoreForHole(myHoleValues[i], myHoleScoreViews[i], color);
+        }
     }
 
     private void displayTotalForPlayer() {
